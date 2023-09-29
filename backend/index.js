4877ee242d6a08b20021468637c60d404d6f91af
Nemuel Wainaina <bR0S3@protonmail.com>
2023-09-29 06:16:50 +0300

    Add realtime chat functionality

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+// chat rooms
+const rooms = {
+    room_1: [],
+    room_2: []
+}
+
+io.on('connection', (socket) => {
+    console.log('New user connected')
+
+    // user joins a room
+    socket.on('joinRoom', ({username, room}) => {
+        socket.join(room)
+        rooms[room].push({id: socket.id, username})
+        io.to(room).emit('newUser', {username, id:socket.id})
+        io.to(room).emit('userList', rooms[room])
+    })
+
+    // handle incoming messages
+    socket.on('chatMessage', ({message, room}) => {
+        io.to(room).emit('newMessage', {message, username:socket.username})
+    })
+
+    // handle user disconnects
+    socket.on('disConnect', () => {
+        console.log('A user disconnected')
+        for(const room in rooms) {
+            const index = rooms[room].indexOf((user) => user.id === socket.id)
+            if(index !== -1) {
+                rooms[room].slice(index, 1) // remove user from users list
+                io.emit('userList', rooms[room])
+            }
+        }
+    })
+})
+
+server.listen(1234, () => {
+    console.log("[*] Server is up and running ...")
+})
